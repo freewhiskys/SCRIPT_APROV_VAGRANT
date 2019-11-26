a6bc5deecfaef219d3a9d7cb6c5ebf183c0da10f
a18sergi <57413299+a18sergi@users.noreply.github.com>
2019-11-26 17:28:10 +0100

    Update conndb.sh

--- conndb.sh
+++ conndb.sh
@@ -3,11 +3,53 @@
 nomscript=$0   # $0 es el nom del programa
 
 function usage () {
-   cat <<EOF
-Usage: nomscript [-u usuari] [-h hostname] [-t]
-   -u   usuari de la base de dades (obligatori)
-   -h   hostname on es connectarà (obligatori
-   -p   port (no obligatori ja que per defecte és 3306, però si existeix ha de ser un numero superior a 1024 i inferior o igual a 65535)
-   -t   no es connecta,  només comprova connexió
-EOF
+   echo "Usage: $0 [-u <string>][-h <string>][-p <3306>]" 1>&2; exit 1;
 }
+
+while getopts ":u:h:p" o; do
+    #echo "OPTIND: ${OPTIND}"
+    case "${o}" in
+        u)
+            USER=${OPTARG}
+            u=${OPTARG}
+            ;;
+			
+        h)
+            HOST=${OPTARG}
+            h=${OPTARG}
+            ;;
+        p)  
+            PORT=${OPTARG}
+            p=${OPTARG}
+            if [ $p -ne 3306 ]
+            then
+				echo "Port 3306"
+				usage
+            fi
+            ;;
+        :)  
+            echo "ERROR: Option -$OPTARG requires an argument"
+            usage
+            ;;
+        \?)
+            echo "ERROR: Invalid option -$OPTARG"
+            usage
+            ;;
+    esac
+	#shift $((OPTIND-1))
+	#printf "Remaining arguments are: %s\n" "$*"
+done
+
+# Check required switches exist
+if [ -z "${u}" ] || [ -z "${h}" ] || [-z "${p}"]
+then
+ usage
+ echo "Please give u, h and p values"
+fi
+
+echo "p = ${p}"
+echo "h = ${h}"
+echo "USER = ${USER}"
+echo "HOST = ${HOST}"
+echo "PORT = ${PORT}"
+echo ${f}
